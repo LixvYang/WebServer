6ef0d4604edc1d9b8754c22bb43fb9b69866b353
lixin <2690688423@qq.com>
2022-08-29 17:08:26 +0800

    update http

--- http_conn.cpp
+++ http_conn.cpp
@@ -1,4 +1,5 @@
 #include "http_conn.h"
+#include <strings.h>
 
 int http_conn::m_epollfd = -1;
 int http_conn::m_user_count = 0;
@@ -16,7 +17,8 @@ void addfd(int epollfd, int fd, bool one_shot)
 {
   epoll_event event;
   event.data.fd = fd;
-  event.events = EPOLLIN | EPOLLRDHUP; 
+  event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+  // event.events = EPOLLIN | EPOLLRDHUP;
 
   if (one_shot)
   {
@@ -51,10 +53,23 @@ void http_conn::init(int sockfd, const sockaddr_in &addr)
   // 端口复用
   int reuse = 1;
   setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-  
+
   // 添加到epoll对象中
   addfd(m_epollfd, sockfd, true);
   m_user_count++;
+  init();
+}
+
+void http_conn::init()
+{
+  m_check_state = CHECK_STATE_REQUESTLINE;
+  m_checked_index = 0;
+  m_start_line = 0;
+  m_read_index = 0;
+  m_method = GET;
+  m_url = 0;
+  m_version = 0;
+  bzero(m_read_buf, READ_BUFFER_SIZE);
 }
 
 // 关闭连接
@@ -70,8 +85,181 @@ void http_conn::close_conn()
 
 bool http_conn::read()
 {
-  printf("一次性读完所有数据");
+  if (m_read_index >= READ_BUFFER_SIZE)
+  {
+    return false;
+  }
+  // 读取到的字节
+  int bytes_read = 0;
+  while (true)
+  {
+    bytes_read = recv(m_sockfd, m_read_buf + m_read_index, READ_BUFFER_SIZE - m_read_index, 0);
+    if (bytes_read == -1)
+    {
+      if (errno == EAGAIN || errno == EWOULDBLOCK)
+      {
+        // 没有数据
+        break;
+      }
+      return false;
+    }
+    else if (bytes_read == 0)
+    {
+      return false;
+    }
+    m_read_index += bytes_read;
+  }
+  printf("读取到了数据,%s\n", m_read_buf);
   return true;
+}
+
+http_conn::HTTP_CODE http_conn::process_read()
+{
+  LINE_STATUS line_status = LINE_OK;
+  HTTP_CODE ret = NO_REQUEST;
+  char *text = 0;
+  while (((m_check_state == CHECK_STATE_CHONNECT) && (line_status == LINE_OK)) || (line_status = parse_line()) == LINE_OK)
+  {
+    // 解析到了一行完整的数据 或者解析到了请求体
+    text = get_line();
+    m_start_line = m_checked_index;
+    printf("got 1 http line :%s\n", text);
+    switch (m_check_state)
+    {
+    case CHECK_STATE_REQUESTLINE:
+    {
+      ret = parse_request_line(text);
+      if (ret == BAD_REQUEST)
+      {
+        return BAD_REQUEST;
+      }
+      break;
+    }
+    case CHECK_STATE_HEADER:
+    {
+      ret = parse_headers(text);
+      if (ret == BAD_REQUEST)
+      {
+        return BAD_REQUEST;
+      }
+      else if (ret == GET_REQUEST)
+      {
+        return do_request();
+      }
+    }
+    case CHECK_STATE_CHONNECT:
+    {
+      ret = parse_content(text);
+      if (ret == GET_REQUEST)
+      {
+        return do_request();
+      }
+      line_status = LINE_OPEN;
+      break;
+    }
+    default:
+    {
+      return INTERNAL_ERROR;
+    }
+    }
+    return NO_REQUEST;
+  }
+
+  return NO_REQUEST;
+}
+
+// 解析http请求 请求方法 ，目标url HTTP版本
+http_conn::HTTP_CODE http_conn::parse_request_line(char *text)
+{
+  // GET /index.html HTTP1.1
+  m_url = strpbrk(text, " \t");
+  *m_url++ = '\0';
+  char *method = text;
+  if (strcasecmp(method, "GET") == 0)
+  {
+    m_method = GET;
+  }
+  else
+  {
+    return BAD_REQUEST;
+  }
+
+  m_version = strpbrk(m_url, " \t");
+  if (!m_version)
+  {
+    return BAD_REQUEST;
+  }
+  *m_version++ = '\0';
+  if (strcasecmp(m_version, "HTTP/1.1") != 0)
+  {
+    return BAD_REQUEST;
+  }
+
+  if (strncasecmp(m_url, "http://", 7) == 0)
+  {
+    m_url += 7;
+    m_url = strchr(m_url, '/');
+  }
+  if (!m_url || m_url[0] != '/')
+  {
+    return BAD_REQUEST;
+  }
+
+  m_check_state = CHECK_STATE_HEADER;
+  return NO_REQUEST;
+}
+
+// 解析请求首行
+http_conn::HTTP_CODE http_conn::parse_headers(char *text)
+{
+  return NO_REQUEST;
+}
+
+//解析请求头
+http_conn::HTTP_CODE http_conn::parse_content(char *text)
+{
+  return NO_REQUEST;
+
+} // 解析请求体
+
+// 解析一行的数据 判断依据\r\n
+http_conn::LINE_STATUS http_conn::parse_line()
+{
+  char temp;
+  for (; m_checked_index < m_read_index; ++m_checked_index)
+  {
+    temp = m_read_buf[m_checked_index];
+    if (temp == '\r')
+    {
+      if ((m_checked_index + 1) == m_read_index)
+      {
+        return LINE_OPEN;
+      }
+      else if (m_read_buf[m_checked_index + 1] == '\n')
+      {
+        m_read_buf[m_checked_index++] = '\0';
+        m_read_buf[m_checked_index++] = '\0';
+        return LINE_OK;
+      }
+      return LINE_BAD;
+    }
+    else if (temp == '\n')
+    {
+      if ((m_checked_index > 1) && (m_read_buf[m_checked_index - 1] == '\r'))
+      {
+        m_read_buf[m_checked_index - 1] = '\0';
+        m_read_buf[m_checked_index++] = '\0';
+        return LINE_OK;
+      }
+      return LINE_BAD;
+    }
+    return LINE_OPEN;
+  }
+  return LINE_OK;
+}
+
+http_conn::HTTP_CODE http_conn::do_request()
+{
 }
 
 bool http_conn::write()
@@ -80,11 +268,25 @@ bool http_conn::write()
   return true;
 }
 
+bool http_conn::process_write(http_conn::HTTP_CODE ret)
+{
+}
+
 // 由线程池中的工作线程调用，这是处理http请求的入口函数
 void http_conn::process()
 {
   // 解析http请求
-  printf("parse request, create response");
-
-  // 生成相应
+  HTTP_CODE read_ret = process_read();
+  if (read_ret == NO_REQUEST)
+  {
+    modfd(m_epollfd, m_sockfd, EPOLLIN);
+    return;
+  }
+  // 生成响应
+  bool write_ret = process_write(read_ret);
+  if (!write_ret)
+  {
+    close_conn();
+  }
+  modfd(m_epollfd, m_sockfd, EPOLLOUT);
 }
--- http_conn.h
+++ http_conn.h
@@ -3,6 +3,7 @@
 
 #include <sys/epoll.h>
 #include <unistd.h>
+#include <string.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -21,8 +22,68 @@
 class http_conn
 {
 public:
-  static int m_epollfd;    // 所有socket上的事件都被注册到一个epoll上
-  static int m_user_count; // 统计用户数量
+  static int m_epollfd;                      // 所有socket上的事件都被注册到一个epoll上
+  static int m_user_count;                   // 统计用户数量
+  static const int READ_BUFFER_SIZE = 2048;  // 读缓冲区列表大小
+  static const int WRITE_BUFFER_SIZE = 1024; //写缓冲区大小
+
+  // http 请求的方法
+  enum METHOD
+  {
+    GET = 0,
+    POST,
+    HEAD,
+    PUT,
+    DELTET,
+    TRACE,
+    OPTIONS,
+    CONNECT
+  };
+
+  // 解析客户请求时，主机的状态
+  // CHECK_STATE_REQUESTLINE 正在分析当前星球行
+  // CHECK_STATE_HEADER
+  // CHECK_STATE_CHONNECT 正在解析请求体
+  enum CHECK_STATE
+  {
+    CHECK_STATE_REQUESTLINE = 0,
+    CHECK_STATE_HEADER,
+    CHECK_STATE_CHONNECT
+  };
+
+  /*
+   * 从状态机的三种可能状态，即行的可读状态，分别表示
+   * 1.读到完整的行 2.行出错 3. 行数据尚不完整
+   */
+  enum LINE_STATUS
+  {
+    LINE_OK = 0,
+    LINE_BAD,
+    LINE_OPEN
+  };
+
+  /*
+   * NO_REQUEST
+   * GET_REQUEST
+   * BAD_REQUEST
+   * NO_RESOURCE
+   * FORBIDDEN_REQUEST
+   * FILE_REQUEST
+   * INTERNAL_ERROR
+   * CLOSED_CONNECTION
+   */
+  enum HTTP_CODE
+  {
+    NO_REQUEST,
+    GET_REQUEST,
+    BAD_REQUEST,
+    NO_RESOURCE,
+    FORBIDDEN_REQUEST,
+    FILE_REQUEST,
+    INTERNAL_ERROR,
+    CLOSED_CONNECTION
+  };
+
   http_conn() {}
   ~http_conn() {}
 
@@ -32,9 +93,38 @@ public:
   bool read();  // 非阻塞读
   bool write(); // 非阻塞写
 
+  LINE_STATUS parse_line();
+
 private:
-  int m_sockfd;          // 该http连接的socket
-  sockaddr_in m_address; // 通信socket地质
+  int m_sockfd;                      // 该http连接的socket
+  sockaddr_in m_address;             // 通信socket地质
+  char m_read_buf[READ_BUFFER_SIZE]; // 读缓冲区
+  int m_read_index;                  // 读缓冲区中已经读入的客户\端数据的最后一个字节的下一个位置
+
+  int m_checked_index; // 当前正在分析的字符在缓冲区的位置
+  int m_start_line;    // 当前正在解析的行的起始位置
+  char *m_url;         // 请求目标的文件名
+  char *m_version;     // 协议版本 http1.1
+  METHOD m_method;     // 请求方法
+  char *m_host;        // 主机名
+  bool m_linger;       // 判断http请求是否要保持连接
+
+  CHECK_STATE m_check_state; // 主状态机目前所处的位置
+
+  void init(); // 初始化连接其他的信息；
+
+  HTTP_CODE process_read();                 // 解析http请求
+  HTTP_CODE parse_request_line(char *text); // 解析请求首行
+  HTTP_CODE parse_headers(char *text);      //解析请求头
+  HTTP_CODE parse_content(char *text);      // 解析请求体
+
+  bool process_write(HTTP_CODE ret);
+
+  char *get_line()
+  {
+    return m_read_buf + m_start_line;
+  }
+  HTTP_CODE do_request();
 };
 
 #endif // !HTTP_CONN_H_
